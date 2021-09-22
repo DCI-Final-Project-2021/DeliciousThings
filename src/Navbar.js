f9658b9e503de55c16f6a4b632ebb4451ce6f6d8
Yahya Yasir Koca <yykoca.08@gmail.com>
2021-09-22 15:13:38 +0200

    sass folder added

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,28 +1,31 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    return (
-        <div>
-            <nav>
-                <div className="logo">Liferando</div>
-                <ul className="nav-links" style={{ transform: open ? "translateX(0px)" : "translateX(-100%)" }}>
-                <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/korb">Korb</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/login">Login</NavLink>
-                    </li>
-                </ul>
-                <i onClick={() => setOpen(!open)} className="fas fa-bars burger"></i>
-            </nav>
-        </div>
-    )
+  return (
+    <div>
+      <nav>
+        <div className="logo">Liferando</div>
+        <ul
+          className="nav-links"
+          style={{ transform: open ? "translateX(0px)" : "translateX(-100%)" }}
+        >
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/korb">Korb</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+        </ul>
+        <i onClick={() => setOpen(!open)} className="fas fa-bars burger"></i>
+      </nav>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
