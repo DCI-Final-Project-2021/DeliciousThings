88d8e82347d26bb1c9c2cc7f706de0eee48819e4
Yahya Yasir Koca <yykoca.08@gmail.com>
2021-09-23 00:02:13 +0200

    category components added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+// import category from "../components/category";
+import menu from "../menu.json";
 
-function Home({products}) {
+function Home() {
+  const [categories, setCategories] = useState([]);
 
-    return (
-        <div>
-            {products.map(product => {
-                return (
-                    <h4>{product.name}</h4>
-                )
-            })}
-        </div>
-    )
+  const cate = [ "burger"]
+
+  useEffect(() => {
+    menu.map((item) => {
+      !categories.includes(item.category) ? categories.push(item.category) : null;
+      setCategories(categories);
+      console.log(categories)
+    });
+  }, []);
+
+  return (
+    <div>
+      {categories.map((category) => {
+        return <h2>{category}</h2>;
+      })}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
