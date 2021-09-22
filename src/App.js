5fdbd59c3a7a6c34392e55dff6eaee89b86f0555
Yahya Yasir Koca <yykoca.08@gmail.com>
2021-09-22 14:44:34 +0200

    products array added and data fetched from api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 import Navbar from './Navbar';
-import { BrowserRouter, Route, Switch } from "react-router-dom"; 
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Korb from './pages/Korb';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import api from './api/fetchDataFromDB';
+import './App.css';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const url = "http://localhost:2005/orders";
+  //url degisecek
+
+  useEffect(() => {
+    api.fetchDataFromDataBase(url)
+      .then(abc => setProducts(abc));
+  }, []);
+
+  console.log(products);
   return (
     <div className="App">
-<BrowserRouter>
-<Navbar />
- <Switch>
-<Route exact path="/" > <Home /> </Route>
-<Route path="/korb"> <Korb /> </Route>
-<Route path="/login"> <Login /> </Route>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" > <Home products={products} /> </Route>
+          <Route path="/korb"> <Korb /> </Route>
+          <Route path="/login"> <Login /> </Route>
 
-</Switch> 
-</BrowserRouter>
-</div> 
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
