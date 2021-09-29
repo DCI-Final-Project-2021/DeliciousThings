5e74361ab031e812307db2a6bb597687b9f1f209
Yahya Yasir Koca <yykoca.08@gmail.com>
2021-09-29 13:39:28 +0200

    formpage added

--- src/pages/Forms.js
+++ src/pages/Forms.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Form, Row, Col, Button } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 
 function Forms() {
@@ -36,59 +35,35 @@ function Forms() {
           Name:
           <input type="text" name="name" />
         </label>
-        <br/>
         <label>
           Surname:
-          <input type="text" name="name" />
+          <input type="text" name="surname" />
         </label>
-        <br/>
+        <label>
+          Email:
+          <input type="email" name="email" />
+        </label>
+        <label>
+          Telefonnummer:
+          <input type="text" name="telefon" />
+        </label>
+        <label>
+          Adress:
+          <input type="text" name="adress" />
+        </label>
+        <label>
+          City:
+          <input type="text" name="city" />
+        </label>
+        <label>
+          City:
+          <input type="text" name="city" />
+        </label>
+        <label>
         <input type="submit" value="Submit" />
+        </label>
       </form>
     </div>
-
-    // <Form className="container form" >
-    //   <Row className="mb-3">
-    //     <Form.Group as={Col} controlId="formGridName">
-    //       <Form.Label>Name</Form.Label>
-    //       <Form.Control type="text" placeholder="Enter name" />
-    //     </Form.Group>
-
-    //     <Form.Group as={Col} controlId="formGridSurname">
-    //       <Form.Label>Surname</Form.Label>
-    //       <Form.Control type="text" placeholder="Enter surname" />
-    //     </Form.Group>
-    //   </Row>
-    //   <Row className="mb-3">
-    //     <Form.Group as={Col} controlId="formGridEmail">
-    //       <Form.Label>Email</Form.Label>
-    //       <Form.Control type="email" placeholder="Enter email" />
-    //     </Form.Group>
-
-    //     <Form.Group as={Col} controlId="formGridPassword">
-    //       <Form.Label>Password</Form.Label>
-    //       <Form.Control type="password" placeholder="Password" />
-    //     </Form.Group>
-    //   </Row>
-
-    //   <Form.Group className="mb-3" controlId="formGridAddress1">
-    //     <Form.Label>Address</Form.Label>
-    //     <Form.Control placeholder="1234 Main St" />
-    //   </Form.Group>
-
-    //   <Form.Group className="mb-3" controlId="formGridAddress2">
-    //     <Form.Label>Address 2</Form.Label>
-    //     <Form.Control placeholder="Apartment, studio, or floor" />
-    //   </Form.Group>
-
-    //   <Form.Group className="mb-3" id="formGridCheckbox">
-    //     <Form.Check type="checkbox" label="Check me out" />
-    //   </Form.Group>
-
-    //   <Button variant="secondary" type="submit">
-    //     Submit
-    //   </Button>
-
-    // </Form>
   );
 }
 
