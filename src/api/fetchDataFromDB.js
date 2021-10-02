ce60a6ad3cfed8b873af719893ee1a8247e8b0f4
Yahya Yasir Koca <yykoca.08@gmail.com>
2021-10-02 11:23:36 +0200

    api functions changed

--- src/api/fetchDataFromDB.js
+++ src/api/fetchDataFromDB.js
@@ -1,12 +1,48 @@
 export default {
-    fetchDataFromDataBase: async function (url) {
-        try {
-            return await fetch(url)
-                .then((response) => response.json())
-                .then((jsonData) => jsonData.result)
-        } catch (error) {
-            console.log(error);
-        }
-    },
-
-}
+  fetchDataFromDataBase: async function (url) {
+    try {
+      return await fetch(url)
+        .then((response) => response.json())
+        .then((jsonData) => jsonData.result);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  createNewCustomer: async function (customer) {
+    console.log("qwqrqwrqrqr", customer);
+    try {
+      const url = `http://localhost:2005/customers`;
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({...customer}),
+      };
+      console.log("create new customer");
+      return await fetch(url, options)
+        .then((response) => response.json())
+        .then((jsonData) => jsonData.result);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  addOrderToCustomer: async function (order) {
+    try {
+      const url = `http://localhost:2005/orders`;
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(order),
+      };
+      console.log("create new customer");
+      return await fetch(url, options)
+        .then((response) => response.json())
+        .then((jsonData) => jsonData.result);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
