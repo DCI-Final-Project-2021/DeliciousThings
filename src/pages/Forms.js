b5eaa22d8465cf71f6b09c54d84b269eb86302d7
Yahya Yasir Koca <yykoca.08@gmail.com>
2021-10-02 11:23:21 +0200

    createNewCustomer function added

--- src/pages/Forms.js
+++ src/pages/Forms.js
@@ -1,32 +1,33 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import api from "../api/fetchDataFromDB";
 
 function Forms() {
   const [customerData, setCustomerData] = useState({
-      name: "",
-      surname: "",
-      email: "",
-      detail: "",
-      tel: "",
-      address: "",
-      city: "",
+    name: "",
+    surname: "",
+    email: "",
+    tel: "",
+    address: "",
+    city: "",
+  });
+  const [order, setOrder] = useState({
+    food: ["Bugünkü siparis"],
+    customerId: "",
+    total: "burayi hesapla",
   });
 
   let history = useHistory();
 
   const submitForm = () => {
-    const url = `http://localhost:2005/orders`;
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(customerData),
-    };
-
-    fetch(url, options)
-      .then((response) => response.json())
-      .then((result) => console.log("Gönderilen Order", result));
+    api.createNewCustomer(customerData).then((result) => {
+      const updatedOrder = { ...order, customerId: result._id };
+      setOrder(updatedOrder);
+      console.log("Olusturulan yeni kullanici:", updatedOrder);
+      api.addOrderToCustomer(updatedOrder).then((result) => {
+        console.log("Customer a siparis eklenmis mi:", result);
+      });
+    });
 
     history.push("/");
   };
@@ -36,30 +37,73 @@ function Forms() {
       <form onSubmit={submitForm}>
         <label>
           Name:
-          <input type="text" name="name" value={customerData.name} onChange={(e)=> setCustomerData({...customerData, name:e.target.value})} />
+          <input
+            type="text"
+            name="name"
+            value={customerData.name}
+            onChange={(e) =>
+              setCustomerData({ ...customerData, name: e.target.value })
+            }
+          />
         </label>
         <label>
           Surname:
-          <input type="text" name="surname" value={customerData.surname} onChange={(e)=> setCustomerData({...customerData, surname:e.target.value})} />
+          <input
+            type="text"
+            name="surname"
+            value={customerData.surname}
+            onChange={(e) =>
+              setCustomerData({ ...customerData, surname: e.target.value })
+            }
+          />
         </label>
         <label>
           Email:
-          <input type="email" name="email" value={customerData.email} onChange={(e)=> setCustomerData({...customerData, email:e.target.value})} />
+          <input
+            type="email"
+            name="email"
+            value={customerData.email}
+            onChange={(e) =>
+              setCustomerData({ ...customerData, email: e.target.value })
+            }
+          />
         </label>
         <label>
           Telefonnummer:
-          <input type="text" name="telefon" value={customerData.tel} onChange={(e)=> setCustomerData({...customerData, tel:e.target.value})} />
+          <input
+            type="text"
+            name="telefon"
+            value={customerData.tel}
+            onChange={(e) =>
+              setCustomerData({ ...customerData, tel: e.target.value })
+            }
+          />
         </label>
         <label>
           Adress:
-          <input type="text" name="adress" value={customerData.address} onChange={(e)=> setCustomerData({...customerData, address:e.target.value})} />
+          <input
+            type="text"
+            name="adress"
+            value={customerData.address}
+            onChange={(e) =>
+              setCustomerData({ ...customerData, address: e.target.value })
+            }
+          />
         </label>
         <label>
           City:
-          <input type="text" name="city" value={customerData.city} onChange={(e)=> setCustomerData({...customerData, city:e.target.value})} />
+          <input
+            type="text"
+            name="city"
+            value={customerData.city}
+            onChange={(e) =>
+              setCustomerData({ ...customerData, city: e.target.value })
+            }
+          />
         </label>
         <label>
           <input type="submit" value="Submit" />
+          <p>ID: {order.customerId}</p>
         </label>
       </form>
     </div>
